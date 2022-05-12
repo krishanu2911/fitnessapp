79c09fea0b6be0a32231a3cf8da843f7ea9f6b4c
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-05-12 23:25:11 +0530

    Fix color contrast

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { QueryDataContextProvider } from "./context/qureyDataContext/QueryDataContext";
+import { QueryDataContextProvider } from "./context/QueryDataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
